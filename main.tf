85bcba94fa7135b850ccd0ffaaae3070bf494dc9
cyrinoorg <sandeep.erande@cyrino.com>
2025-09-26 15:01:30 +0530

    Update main Terraform configuration for EC2 module

--- main.tf
+++ main.tf
@@ -1,13 +1,27 @@
-module "ec2_instance" {
-  source = "terraform-aws-modules/ec2-instance/aws"
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  default     = "t2.micro"
+}
 
-  name = "example-instance"
+variable "ami" {
+  description = "AMI ID for the instance"
+}
 
-  ami           = "ami-0c55b159cbfafe1f0" # Replace with your desired AMI ID
-  instance_type = "t2.micro"
+variable "key_name" {
+  description = "Key pair name for SSH access"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for the instance"
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
 
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name = "Terraform-EC2"
   }
 }
